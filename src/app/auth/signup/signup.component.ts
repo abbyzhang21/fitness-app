71990fe2e552cc629379da90f442fad300730df5
abby <abbyzhang21@yahoo.com>
2019-09-19 10:54:04 -0700

    'angular-material'

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+//using template approach - form
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  maxDate;
+  constructor() { }
+
+  ngOnInit() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
+}
